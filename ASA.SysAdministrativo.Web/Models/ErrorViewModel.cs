d1b4380608b1c03f64b41cd640e7bd2c524cac99
Hector Garcia <hg425823@gmail.com>
2024-02-27 11:16:45 -0600

    Inclusion de Arquitectura de N-Capas a Utilizar

---
+++ ASA.SysAdministrativo.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASA.SysAdministrativo.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
