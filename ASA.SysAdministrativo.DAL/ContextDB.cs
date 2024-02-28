219721e3e016b6de940326d84bf5c9657fa0cac7
Hector Garcia <hg425823@gmail.com>
2024-02-28 10:38:50 -0600

    Configuracion de la DBContext

---
+++ ASA.SysAdministrativo.DAL/ContextDB.cs
@@ -0,0 +1,26 @@
+﻿#region REFERENCIAS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Referencias Necesarias Para El Correcto Funcionamiento
+using Microsoft.EntityFrameworkCore;
+
+#endregion
+
+namespace ASA.SysAdministrativo.DAL
+{
+    public class ContextDB : DbContext
+    {
+        #region REFERENCIAS DE TABLAS DE LA BD
+
+        #endregion
+
+        // Metodo de configuracion a la conexion a la base de datos
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@""); //Poner str de concexion local o remota
+        }
+    }
+}
