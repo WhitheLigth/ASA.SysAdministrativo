a7b16b8a06948ebcc727e08e82468da2a8408513
Hector Garcia <hg425823@gmail.com>
2024-02-29 11:55:33 -0600

    Creacion de la clase de Entidades Empleados

---
+++ ASA.SysAdministrativo.EN/Empleados EN/Empleados.cs
@@ -0,0 +1,20 @@
+﻿#region REFERENCIAS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Referencias Necesarias Para El Correcto Funcionamiento
+
+
+#endregion
+
+namespace ASA.SysAdministrativo.EN.Empleados_EN
+{
+    public class Empleados
+    {
+        #region Atributos para Empleados
+
+        #endregion
+    }
+}
