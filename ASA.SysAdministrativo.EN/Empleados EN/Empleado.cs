83231d74618d1871242f89889898d90d08933053
Hector Garcia <hg425823@gmail.com>
2024-03-01 11:17:03 -0600

    Modificacion de nombre a la clase de entidades Empleado

--- ASA.SysAdministrativo.EN/Empleados EN/Empleado.cs
+++ ASA.SysAdministrativo.EN/Empleados EN/Empleado.cs
@@ -11,7 +11,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ASA.SysAdministrativo.EN.Empleados_EN
 {
-    public class Empleados
+    public class Empleado
     {
         #region Atributos para Empleados
         [Key]
