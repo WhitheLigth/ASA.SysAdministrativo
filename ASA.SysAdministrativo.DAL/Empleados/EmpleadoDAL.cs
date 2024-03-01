bbc5c7e4aa564b954a9caa01a25d9a3ea46ead96
Hector Garcia <hg425823@gmail.com>
2024-03-01 11:17:43 -0600

    Cracion de Clase Acceso a Datos junto al metodo Create - EmpleadoDAL

---
+++ ASA.SysAdministrativo.DAL/Empleados/EmpleadoDAL.cs
@@ -0,0 +1,31 @@
+﻿#region REFERENCIAS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Referencias Necesarias Para El Correcto Funcionamiento
+using ASA.SysAdministrativo.EN.Empleados_EN;
+
+
+#endregion
+
+namespace ASA.SysAdministrativo.DAL.Empleados
+{
+    public class EmpleadoDAL
+    {
+        #region METODO PARA GUARDAR
+        public static async Task<int> CrearEmpleadoAsync (Empleado pEmpleados)
+        {
+            int result = 0;
+            // Un bloque de conexion que mientras se permanezca en el bloque la base de datos permanecera abierta y al terminar se destruira
+            using (var dbContext = new ContextDB())
+            {
+                dbContext.Add(pEmpleados);
+                result = await dbContext.SaveChangesAsync(); // Await sirve para esperar a terminar todos los procesos para devolverlos todos juntos
+            }
+            return result;  // Si se realizo con exito devuelve 1 sino devuelve 0
+        }
+        #endregion
+    }
+}
